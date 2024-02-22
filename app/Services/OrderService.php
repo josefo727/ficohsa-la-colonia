6cb0fd31538b21f7801a39872e372aab98730297
José Rafael Gutierrez <josefo727@gmail.com>
2024-02-22 16:28:30 -0500

    Fix: fix general setting seeder error

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,51 +3,48 @@
 namespace App\Services;
 
 use App\Adapters\VtexAdapter;
-use Carbon\Carbon;
-use Josefo727\GeneralSettings\Models\GeneralSetting;
 use App\Classes\Status;
 use App\Models\Order;
-use App\Services\PixelPayService;
+use Carbon\Carbon;
+use Josefo727\GeneralSettings\Models\GeneralSetting;
 
 class OrderService
 {
     protected string $orderId;
+
     protected string $paymentSystemKey;
+
     protected string $currencyKey;
+
     protected string $urlRedirect;
 
     protected array $response = [
         'found' => false,
         'needs_to_process' => false,
         'message' => 'Orden no encontrada',
-        'url' => null
+        'url' => null,
     ];
 
-    public function __construct(string $orderId) {
+    public function __construct(string $orderId)
+    {
         $this->orderId = $orderId;
         $this->paymentSystemKey = config('vtex.order.payment_system_key');
         $this->currencyKey = config('vtex.order.currency_key');
         $this->urlRedirect = (new DomainUrlService())->generate(request())
-            . '/checkout/orderPlaced?og='
-            . $this->getOrderGroup($orderId);
+            .'/checkout/orderPlaced?og='
+            .$this->getOrderGroup($orderId);
     }
 
-    /**
-     * @return string|null
-     */
-    public function getOrderGroup(string $orderId): string|null
+    public function getOrderGroup(string $orderId): ?string
     {
         return preg_replace('/-(\d+)/', '', $orderId);
     }
 
-    /**
-     * @return array
-     */
     public function getOrderInfo(): array
     {
         $info = app(VtexAdapter::class)->getOrder($this->orderId);
 
-        if (!$info['success']) {
+        if (! $info['success']) {
             return $this->response;
         }
 
@@ -82,40 +79,38 @@ class OrderService
         return $this->response;
     }
 
-    /**
-     * @param array $order
-     * @return bool
-     */
     public function needsToBeProcessed(array $order): bool
     {
         $paymentSystem = (string) data_get($order, $this->paymentSystemKey);
-		$vtexPaymentSystems = GeneralSetting::getValue('VTEX_PAYMENT_SYSTEM');
+        $vtexPaymentSystems = GeneralSetting::getValue('VTEX_PAYMENT_SYSTEM');
+
         return is_array($vtexPaymentSystems) && in_array($paymentSystem, $vtexPaymentSystems)
             && $order['status'] === Status::ACTIONABLE_VTEX_STATE;
     }
 
     /**
+     * @param  array  $order
      * @return array<string,mixed>
-     * @param array $order
      */
     public function buildNewOrder($order): array
     {
         $client = data_get($order, 'clientProfileData');
         $email = $this->getClientEmail($client['userProfileId']);
+
         return [
             'order_id' => $order['orderId'],
-            'complete' => urlencode($this->urlRedirect . '&complete=true'),
-            'cancel' => urlencode($this->urlRedirect . '&cancel=true'),
+            'complete' => urlencode($this->urlRedirect.'&complete=true'),
+            'cancel' => urlencode($this->urlRedirect.'&cancel=true'),
             'currency' => data_get($order, $this->currencyKey),
             'amount' => $order['value'] / 100,
             'email' => $email,
             'first_name' => $client['firstName'],
             'last_name' => $client['lastName'],
-            'callback' => urlencode(config('app.url') . '/api/transaction-result/' . $order['orderId']),
+            'callback' => urlencode(config('app.url').'/api/transaction-result/'.$order['orderId']),
             'vtex_status' => $order['status'],
             'status' => Status::NEEDS_APPROVAL,
             'needs_approval' => true,
-            'order_creation_at' => Carbon::parse($order['creationDate'])->format('Y-m-d H:m:i')
+            'order_creation_at' => Carbon::parse($order['creationDate'])->format('Y-m-d H:m:i'),
         ];
     }
 
--- database/seeders/GeneralSettingSeeder.php
+++ database/seeders/GeneralSettingSeeder.php
@@ -17,61 +17,61 @@ class GeneralSettingSeeder extends Seeder
                 'name' => 'PIXELPAY_URL_BASE',
                 'value' => 'https://pixelpay.app/api/v2/transaction/hosted/sandbox',
                 'description' => 'Url base del servicio pixelpay.',
-                'type' => 'url'
+                'type' => 'url',
             ],
             [
                 'name' => 'PIXELPAY_KEY_ID',
                 'value' => '1234567890',
                 'description' => 'Key o Key ID de pixelpay.',
-                'type' => 'password'
+                'type' => 'password',
             ],
             [
                 'name' => 'PIXELPAY_HASH',
                 'value' => '@s4ndb0x-abcd-1234-n1l4-p1x3l',
                 'description' => 'Hash de pixelpay.',
-                'type' => 'password'
+                'type' => 'password',
             ],
             [
                 'name' => 'VTEX_ACCOUNT_NAME',
-                'value' => 'massivespace',
+                'value' => 'massivespacenew',
                 'description' => 'Nombre de la cuenya en Vtex.',
-                'type' => 'string'
+                'type' => 'string',
             ],
             [
                 'name' => 'VTEX_ENVIRONMENT',
                 'value' => 'vtexcommercestable',
                 'description' => 'Entorno API Vtex.',
-                'type' => 'string'
+                'type' => 'string',
             ],
             [
                 'name' => 'VTEX_APP_KEY',
-                'value' => 'vtexappkey-massivespace-GMORVY',
+                'value' => 'vtexappkey-massivespacenew-TXNDVP',
                 'description' => 'App key de Vtex.',
-                'type' => 'password'
+                'type' => 'password',
             ],
             [
                 'name' => 'VTEX_APP_TOKEN',
-                'value' => 'FVZCTBMFTHUHYAFMWTLONYIWSEJXQBUXPZGJIVISGBKPHFTEHJLSCAZGREIPSNRNVLSBYAJKQWPWXJOSBCRMIQESKLFLXMUJPSFZZLURXHKHWBCGLLRHVLVCKJDFHZRK',
+                'value' => 'EPYJLVTYILXHWGHPVFNPZKALRKJNRLNTJQVVPSROBYAJGIPGDJPQRAHKWVTGENBKANPBGCNCXGDGKGOQZPKFRSQNSGAYGYIDHYVHQJQETIOCNWADWFMWSKJIJPTDBSLW',
                 'description' => 'App token de Vtex.',
-                'type' => 'password'
+                'type' => 'password',
             ],
             [
                 'name' => 'VTEX_PAYMENT_SYSTEM',
                 'value' => '17',
                 'description' => 'Código de pixelpay en Vtex.',
-                'type' => 'string'
+                'type' => 'array',
             ],
             [
                 'name' => 'VTEX_MASTER_DOMAIN',
-                'value' => 'https://massivespace.myvtex.com',
+                'value' => 'https://massivespacenew.myvtex.com',
                 'description' => 'Dominio master tienda vtex.',
-                'type' => 'string'
+                'type' => 'string',
             ],
             [
                 'name' => 'VTEX_PRODUCTION_DOMAIN',
-                'value' => 'https://massivespace.myvtex.com',
+                'value' => 'https://massivespacenew.myvtex.com',
                 'description' => 'Dominio de producción tienda vtex.',
-                'type' => 'string'
+                'type' => 'string',
             ],
 
         ];
